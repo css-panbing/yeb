d56a4c59efe0abd6683d1bcf6c9872d257b9db21
panbing <1502842065@qq.com>
2022-04-03 15:46:59 +0800

    解决Redis序列化LocalDateTime异常的问题

--- yeb-server/src/main/java/com/cssnj/server/config/RedisConfig.java
+++ yeb-server/src/main/java/com/cssnj/server/config/RedisConfig.java
@@ -1,10 +1,16 @@
 package com.cssnj.server.config;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 /**
@@ -18,15 +24,23 @@ public class RedisConfig {
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory){
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<Object>(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        //解决jackson2无法序列化LocalDateTime的问题
+        om.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        om.registerModule(new JavaTimeModule());
+
+        jackson2JsonRedisSerializer.setObjectMapper(om);
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         // String类型 key序列器
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         // String类型 value序列器
-        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
         // Hash类型 key序列器
         redisTemplate.setHashKeySerializer(new StringRedisSerializer());
         // Hash类型 value序列器
-        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
         redisTemplate.setConnectionFactory(redisConnectionFactory);
         return redisTemplate;
     }
